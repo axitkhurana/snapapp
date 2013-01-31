91c63b6eac62e2d11055c75a67706b995a0ca10e
Akshit Khurana <axitkhurana@gmail.com>
2013-02-01 03:49:17 +0530

    add templates, serve media

--- foreversnap/foreversnap/urls.py
+++ foreversnap/foreversnap/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,3 +7,8 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^snapapp/', include('snapapp.urls')),
                        )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+         'document_root': settings.MEDIA_ROOT}))
